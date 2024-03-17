b720b0abafc221ac4da06f4b538f3b8ab1d27b6b
DESKTOP-GFN0QL2\DELL <prajapatianoop645@gmail.com>
2024-03-17 19:05:02 +0530

    Intial checkin

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Anoop Prajapati</h1>
+     <h2>Anoop Prajapati</h2>
+     <h3>Anoop Prajapati</h3>
+     <h4>Anoop Prajapati</h4>
+     <h5>Anoop Prajapati</h5>
+     <h6>Anoop Prajapati</h6>
+    </div>
+  );
+}
+
+export default App;
